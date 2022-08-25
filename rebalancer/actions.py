07727fab10f2ce2ec5a8b6f2244990250b41b7ce
maciejnems <macieknems@gmail.com>
2022-08-25 13:03:28 +0200

    implement basic rebalancer actions

--- rebalancer/actions.py
+++ rebalancer/actions.py
@@ -1,10 +1,52 @@
-import imp
-from rebalancer.formulas import amount_out
+from rebalancer import formulas
 from rebalancer.model import Token
 
 
-def swap(a_in: float, t_in: Token, t_out: Token):
-    a_out = amount_out(a_in, t_in, t_out)
-    t_in.balance += a_in
-    t_out.balance -= a_out
-    return t_in, t_out, a_out
+def swap(state, user_register, a_in: float, t_in: str, t_out: str):
+    a_out = formulas.amount_out(a_in, state[t_in], state[t_out]) * 0.95
+    state[t_in].balance += a_in
+    state[t_out].balance -= a_out
+    print("swap", state, a_in, t_in, t_out, a_out)
+    return state
+
+
+def provide_liquidity(state, user_register, deposit: float, name: str, user: str):
+    C = formulas.compute_C(state)
+    supply = formulas.get_issued(name, deposit, C, state)
+    state[name].supply += supply
+    target_ratios = formulas.new_target_ratios(
+        name, deposit, C, state)
+    if user not in user_register:
+        user_register[user] = {}
+    if name in user_register[user]:
+        user_register[user][name] += supply
+    else:
+        user_register[user][name] = supply
+    state[name].balance += deposit
+    for name, tr in target_ratios.items():
+        state[name].target_ratio = tr
+    return state
+
+
+def remove_liquidity(state, user_register, redeemed: float, name: str, user: str):
+    if user not in user_register or name not in user_register[user]:
+        print(f"No entry for user {user} for token {name}")
+        return 0
+    if redeemed is None:
+        redeemed = user_register[user][name]
+    if user_register[user][name] < redeemed:
+        print(f"User does not have enough founds. Tried to withdraw {redeemed}, has {user_register[user][name]}")
+        return 0
+    C = formulas.compute_C(state)
+    withdrawal = formulas.get_withdrawal(name, redeemed, C, state)
+    if withdrawal > state[name].balance:
+        print(f"Not enough tokens to withdraw {withdrawal} of token {name}")
+        return 0
+    state[name].supply -= redeemed
+    user_register[user][name] -= redeemed
+    state[name].balance -= withdrawal
+    target_ratios = formulas.new_target_ratios(
+        name, -withdrawal, C, state)
+    for name, tr in target_ratios.items():
+        state[name].target_ratio = tr
+    return state
--- rebalancer/formulas.py
+++ rebalancer/formulas.py
@@ -2,7 +2,54 @@ from rebalancer.model import Token
 import math
 
 
-def amount_out(a_in: float, t_in: Token, t_out: Token):
+def amount_out(a_in: float, t_in: Token, t_out: Token) -> float:
     return t_out.balance * \
-        (1-(t_in.balance / math.exp(t_in.balance +
-         a_in, t_in.target_ratio / t_out.target_ratio)))
+        (1-math.pow(t_in.balance / (t_in.balance +
+         a_in), t_in.target_ratio / t_out.target_ratio))
+
+
+def target_balance_denominator(tokens: dict) -> float:
+    return math.prod(math.pow(t.target_ratio/t.price, t.target_ratio) for t in tokens.values())
+
+
+def target_balances(C: float, tokens: dict) -> dict:
+    prod = target_balance_denominator(tokens)
+    return {t.name: (t.target_ratio * C / t.price) / prod for t in tokens.values()}
+
+
+def target_balance(name: str, C: float, tokens: dict) -> float:
+    TB = tokens[name].target_ratio * C / tokens[name].price
+    print(TB)
+    prod = target_balance_denominator(tokens)
+    print(prod)
+    return TB / prod
+
+
+def new_target_ratios(name: str, diff: float, C: float, tokens: dict) -> dict:
+    TB = target_balance(name, C, tokens)
+    multiplier = TB / (TB + diff * tokens[name].target_ratio)
+    ratios = {t.name: t.target_ratio *
+              multiplier for t in tokens.values() if t.name is not name}
+    ratios[name] = tokens[name].target_ratio * (1 + diff/TB) * multiplier
+    return ratios
+
+
+def compute_C(tokens: dict) -> float:
+    return math.prod(math.pow(t.balance, t.target_ratio) for t in tokens.values())
+
+
+#             Deposit_i * Supply_i   
+# P_issued = ----------------------
+#                      TB_i
+def get_issued(name: str, deposit: float, C: float, tokens: dict) -> float:
+    TB = target_balance(name, C, tokens)
+    return deposit * tokens[name].supply / TB
+
+
+#               P_redeemed * TB_i   
+# Withdraw_i = -------------------
+#                    Supply_i
+def get_withdrawal(name: str, redeemed: float, C: float, tokens: dict) -> float:
+    TB = target_balance(name, C, tokens)
+    return redeemed * TB / tokens[name].supply
+
