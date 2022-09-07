940ce64973e4a3cacf6a6cc486b178cda06452c7
maciejnems <macieknems@gmail.com>
2022-09-07 22:54:56 +0200

    fix bug

--- simulations/simulate.py
+++ simulations/simulate.py
@@ -3,9 +3,11 @@ from cadCAD.configuration import Experiment
 from rebalancer import state_updates, policies,  utils, genesis
 from rebalancer.names import POOL, BLOCK, PROFIT, POPULARITY, TRADING_VOLUME
 
+REBALANCE = True
+# REBALANCE = False
+
 historical_data = utils.get_historical_data()
-genesis_state = genesis.get_state_from_historical_data(
-    historical_data)
+genesis_state = genesis.get_state_from_historical_data(historical_data)
 
 user_record = {
     "root": {
@@ -20,7 +22,7 @@ psubs = [
         },
         "variables": {
             PROFIT: state_updates.profit_update,
-            POOL: state_updates.get_pool_state_upadate(user_record, historical_data),
+            POOL: state_updates.get_pool_state_upadate(user_record, historical_data, REBALANCE),
             BLOCK: state_updates.block_update,
             POPULARITY: state_updates.get_popularity_update(historical_data),
             TRADING_VOLUME: state_updates.trading_volume_update,
